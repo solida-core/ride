2bf2dd3e4e25481b1aec6f3527d2a62a5d77326b
Gianmauro Cuccuru <gmauro@crs4.it>
2018-01-17 00:20:52 +0100

    bugfix

--- rules/scripts/featureCounts_script.py
+++ rules/scripts/featureCounts_script.py
@@ -1,18 +1,20 @@
 from subprocess import run
 
-if len(snakemake.input[0]) > 1:
+if len(snakemake.input) > 2:
     cmd = [snakemake.params['cmd'],
            '-p',
            '-T', str(snakemake.threads),
-           '-t', str(snakemake.params['gtf_feature_type']),
-           '-a', str(snakemake.params['gtf']),
-           '-o', snakemake.output,
-           snakemake.input[1]]
+           '-t', snakemake.params['gtf_feature_type'],
+           '-a', snakemake.params['gtf'][0],
+           '-g', snakemake.params['gtf_attribute_type'],
+           '-o', snakemake.output[0],
+           snakemake.input['bam']]
 else:
     cmd = [snakemake.params['cmd'],
            '-T', str(snakemake.threads),
-           '-t', str(snakemake.params['gtf_feature_type']),
-           '-a', str(snakemake.params['gtf']),
-           '-o', snakemake.output,
-           snakemake.input[1]]
+           '-t', snakemake.params['gtf_feature_type'],
+           '-a', snakemake.params['gtf'],
+           '-g', snakemake.params['gtf_attribute_type'],
+           '-o', snakemake.output[0],
+           snakemake.input['bam']]
 run(cmd)
